fa985205b83fafa866a58ed21b3516acb3f1c18b
Rachael <rachaelewins@hotmail.com>
2020-05-18 16:28:09 +0100

    refactor by reducing line length via new variable

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,7 +8,8 @@ class Oystercard
   end
 
   def top_up(num)
-    fail "The maximum of #{MAXIMUM_BALANCE} would be exceeded." if balance + num > MAXIMUM_BALANCE
+    message = "The maximum of #{MAXIMUM_BALANCE} would be exceeded."
+    fail message if balance + num > MAXIMUM_BALANCE
     @balance += num
   end
 
