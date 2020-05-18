acfdfb60a0c79148161974051a1296fe0941abc5
Sophie <sophiebrown438@gmail.com>
2020-05-18 14:48:09 +0100

    New card, 0 balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,7 @@
-describe "doesn't exist yet" do
-  
+require 'oystercard'
+describe Oystercard do
+  it "should have a default balance of zero" do
+    card = Oystercard.new
+    expect(card.balance).to eq 0
+  end
 end
